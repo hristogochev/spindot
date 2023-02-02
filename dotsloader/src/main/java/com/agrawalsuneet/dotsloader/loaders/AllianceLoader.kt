68df3b77cfe70960309f4b9d0b83637c5cead850
hristogochev <hristogochev35@gmail.com>
2023-02-02 07:29:43 +0200

    Refactored AllianceLoader.kt

--- dotsloader/src/main/java/com/agrawalsuneet/dotsloader/loaders/AllianceLoader.kt
+++ dotsloader/src/main/java/com/agrawalsuneet/dotsloader/loaders/AllianceLoader.kt
@@ -3,69 +3,73 @@ package com.agrawalsuneet.dotsloader.loaders
 import android.content.Context
 import android.util.AttributeSet
 import android.view.Gravity
-import android.view.ViewTreeObserver
+import android.view.View
 import android.view.animation.AccelerateDecelerateInterpolator
-import android.view.animation.Animation
 import android.view.animation.TranslateAnimation
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import com.agrawalsuneet.dotsloader.R
 import com.agrawalsuneet.dotsloader.basicviews.CircleView
-import com.agrawalsuneet.dotsloader.contracts.LoaderContract
+import com.agrawalsuneet.dotsloader.contracts.AnimationContract
+import com.agrawalsuneet.dotsloader.utils.getColorResource
+import com.agrawalsuneet.dotsloader.utils.onAnimationEnd
 
 /**
  * Created by agrawalsuneet on 9/1/18.
+ * Modifier by hristogochev on 02/02/23.
  */
 
-class AllianceLoader : LinearLayout, LoaderContract {
+class AllianceLoader : LinearLayout, AnimationContract {
+    // Default input attributes
+    private val defaultDotsRadius = 50
+    private val defaultDistanceMultiplier = 4
+    private val defaultFirstColor = getColorResource(R.color.loader_selected)
+    private val defaultSecondColor = getColorResource(R.color.loader_selected)
+    private val defaultThirdColor = getColorResource(R.color.loader_selected)
+    private val defaultDrawOnlyStroke = false
+    private val defaultStrokeWidth = 20
+    private val defaultAnimDuration = 500
+    private val defaultToggleOnVisibilityChange = true
 
 
-    var dotsRadius: Int = 50
-    var strokeWidth: Int = 0
-
-    var drawOnlyStroke: Boolean = false
-
-    var distanceMultiplier: Int = 4
+    // Settable attributes
+    private var dotsRadius = defaultDotsRadius
+    private var distanceMultiplier = defaultDistanceMultiplier
         set(value) {
-            if (value < 1) {
-                field = 1
-            } else {
-                field = value
-            }
+            field = if (value < 1) 1 else value
         }
+    private var firsDotColor = defaultFirstColor
+    private var secondDotColor = defaultSecondColor
+    private var thirdDotColor = defaultThirdColor
+    private var drawOnlyStroke = defaultDrawOnlyStroke
+    private var strokeWidth = defaultStrokeWidth
+    private var animDuration = defaultAnimDuration
+    private var toggleOnVisibilityChange = defaultToggleOnVisibilityChange
 
-    var firsDotColor: Int = resources.getColor(android.R.color.holo_red_dark)
-    var secondDotColor: Int = resources.getColor(android.R.color.holo_green_dark)
-    var thirdDotColor: Int = resources.getColor(R.color.loader_selected)
-
-    var animDuration: Int = 500
-
-    private var step: Int = 0
-
-    private var calWidthHeight: Int = 0
+    // Views
     private lateinit var firstCircle: CircleView
     private lateinit var secondCircle: CircleView
     private lateinit var thirdCircle: CircleView
     private lateinit var relativeLayout: RelativeLayout
 
-
+    // Animation attributes
+    private var step: Int = 0
+    private var calWidthHeight: Int = 0
     private var posArrayList: ArrayList<ArrayList<Pair<Float, Float>>> = ArrayList()
+    private var animationStopped = false
 
-    constructor(context: Context) : super(context) {
-        initView()
-    }
-
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
-        initAttributes(attrs)
-        initView()
-    }
-
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
-        initAttributes(attrs)
-        initView()
-    }
-
-    constructor(context: Context?, dotsRadius: Int, distanceMultiplier: Int, drawOnlyStroke: Boolean, strokeWidth: Int, firsDotColor: Int, secondDotColor: Int, thirdDotColor: Int) : super(context) {
+    // Custom constructors
+    constructor(
+        context: Context?,
+        dotsRadius: Int,
+        distanceMultiplier: Int,
+        drawOnlyStroke: Boolean,
+        strokeWidth: Int,
+        firsDotColor: Int,
+        secondDotColor: Int,
+        thirdDotColor: Int,
+        toggleOnVisibilityChange: Boolean
+    ) : super(context) {
         this.dotsRadius = dotsRadius
         this.distanceMultiplier = distanceMultiplier
         this.drawOnlyStroke = drawOnlyStroke
@@ -73,131 +77,157 @@ class AllianceLoader : LinearLayout, LoaderContract {
         this.firsDotColor = firsDotColor
         this.secondDotColor = secondDotColor
         this.thirdDotColor = thirdDotColor
-
+        this.toggleOnVisibilityChange = toggleOnVisibilityChange
         initView()
+        initInitialValues()
     }
 
-    override fun initAttributes(attrs: AttributeSet) {
+    // Default constructors
+    constructor(context: Context) : super(context) {
+        initView()
+        initInitialValues()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        initAttributes(attrs)
+        initView()
+        initInitialValues()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        initAttributes(attrs)
+        initView()
+        initInitialValues()
+    }
+
+
+    // Initialization functions
+    private fun initAttributes(attrs: AttributeSet) {
         val typedArray = context.obtainStyledAttributes(attrs, R.styleable.AllianceLoader, 0, 0)
 
-        this.dotsRadius = typedArray.getDimensionPixelSize(R.styleable.AllianceLoader_alliance_dotsRadius, 50)
-
-        this.distanceMultiplier = typedArray.getInteger(R.styleable.AllianceLoader_alliance_distanceMultiplier, 4)
-
-        this.firsDotColor = typedArray.getColor(R.styleable.AllianceLoader_alliance_firstDotsColor,
-                resources.getColor(R.color.loader_selected))
-
-        this.secondDotColor = typedArray.getColor(R.styleable.AllianceLoader_alliance_secondDotsColor,
-                resources.getColor(R.color.loader_selected))
-
-        this.thirdDotColor = typedArray.getColor(R.styleable.AllianceLoader_alliance_thirdDotsColor,
-                resources.getColor(R.color.loader_selected))
-
-        this.drawOnlyStroke = typedArray.getBoolean(R.styleable.AllianceLoader_alliance_drawOnlyStroke, false)
-
+        this.dotsRadius = typedArray.getDimensionPixelSize(
+            R.styleable.AllianceLoader_alliance_dotsRadius,
+            defaultDotsRadius
+        )
+        this.distanceMultiplier = typedArray.getInteger(
+            R.styleable.AllianceLoader_alliance_distanceMultiplier,
+            defaultDistanceMultiplier
+        )
+        this.firsDotColor = typedArray.getColor(
+            R.styleable.AllianceLoader_alliance_firstDotsColor,
+            defaultFirstColor
+        )
+        this.secondDotColor = typedArray.getColor(
+            R.styleable.AllianceLoader_alliance_secondDotsColor,
+            defaultSecondColor
+        )
+        this.thirdDotColor = typedArray.getColor(
+            R.styleable.AllianceLoader_alliance_thirdDotsColor,
+            defaultThirdColor
+        )
+        this.drawOnlyStroke = typedArray.getBoolean(
+            R.styleable.AllianceLoader_alliance_drawOnlyStroke,
+            defaultDrawOnlyStroke
+        )
         if (drawOnlyStroke) {
-            this.strokeWidth = typedArray.getDimensionPixelSize(R.styleable.AllianceLoader_alliance_strokeWidth, 20)
+            this.strokeWidth = typedArray.getDimensionPixelSize(
+                R.styleable.AllianceLoader_alliance_strokeWidth,
+                defaultStrokeWidth
+            )
         }
+        this.animDuration =
+            typedArray.getInt(R.styleable.AllianceLoader_alliance_animDuration, defaultAnimDuration)
 
-        this.animDuration = typedArray.getInt(R.styleable.AllianceLoader_alliance_animDuration, 500)
+        this.toggleOnVisibilityChange = typedArray.getBoolean(
+            R.styleable.AllianceLoader_alliance_toggleOnVisibilityChange,
+            defaultToggleOnVisibilityChange
+        )
 
         typedArray.recycle()
     }
 
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-
-        if (calWidthHeight == 0) {
-            calWidthHeight = (2 * dotsRadius * distanceMultiplier) + strokeWidth
-        }
-
-        setMeasuredDimension(calWidthHeight, calWidthHeight)
-    }
-
     private fun initView() {
-        removeAllViews()
-        removeAllViewsInLayout()
 
         this.gravity = Gravity.CENTER_HORIZONTAL
 
-        relativeLayout = RelativeLayout(context)
-        relativeLayout.gravity = Gravity.CENTER_HORIZONTAL
-
+        relativeLayout = RelativeLayout(context).apply {
+            gravity = Gravity.CENTER_HORIZONTAL
+        }
 
         if (calWidthHeight == 0) {
             calWidthHeight = (2 * dotsRadius * distanceMultiplier) + strokeWidth
         }
 
         firstCircle = CircleView(context, dotsRadius, firsDotColor, drawOnlyStroke, strokeWidth)
-        val firstParam = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT)
-        firstParam.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE)
-        firstParam.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE)
-
-        relativeLayout.addView(firstCircle, firstParam)
+        val firstParam = RelativeLayout.LayoutParams(
+            RelativeLayout.LayoutParams.WRAP_CONTENT,
+            RelativeLayout.LayoutParams.WRAP_CONTENT
+        ).apply {
+            addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE)
+            addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE)
+        }
 
         secondCircle = CircleView(context, dotsRadius, secondDotColor, drawOnlyStroke, strokeWidth)
-        val secondParam = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT)
-        secondParam.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE)
-        secondParam.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE)
-
-        relativeLayout.addView(secondCircle, secondParam)
+        val secondParam = RelativeLayout.LayoutParams(
+            RelativeLayout.LayoutParams.WRAP_CONTENT,
+            RelativeLayout.LayoutParams.WRAP_CONTENT
+        ).apply {
+            addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE)
+            addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE)
+        }
 
         thirdCircle = CircleView(context, dotsRadius, thirdDotColor, drawOnlyStroke, strokeWidth)
-        val thirdParam = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT)
-        thirdParam.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE)
-        thirdParam.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE)
+        val thirdParam = RelativeLayout.LayoutParams(
+            RelativeLayout.LayoutParams.WRAP_CONTENT,
+            RelativeLayout.LayoutParams.WRAP_CONTENT
+        ).apply {
+            addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE)
+            addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE)
+        }
 
+        relativeLayout.addView(firstCircle, firstParam)
+        relativeLayout.addView(secondCircle, secondParam)
         relativeLayout.addView(thirdCircle, thirdParam)
 
         val relParam = RelativeLayout.LayoutParams(calWidthHeight, calWidthHeight)
         this.addView(relativeLayout, relParam)
-
-        initInitialValues()
-
-
-        val loaderView = this
-
-
-        viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
-            override fun onGlobalLayout() {
-                startLoading()
-
-                val vto = loaderView.viewTreeObserver
-                vto.removeOnGlobalLayoutListener(this)
-            }
-        })
     }
 
     private fun initInitialValues() {
-
         val fullDistance = (calWidthHeight - ((2 * dotsRadius) + strokeWidth)).toFloat()
         val halfDistance = fullDistance / 2
 
-        val firstPosArray = ArrayList<Pair<Float, Float>>()
-        firstPosArray.add(Pair(0.0f, 0.0f))
-        firstPosArray.add(Pair(halfDistance, fullDistance))
-        firstPosArray.add(Pair(-halfDistance, fullDistance))
+        val firstPosArray = ArrayList<Pair<Float, Float>>().apply {
+            add(Pair(0.0f, 0.0f))
+            add(Pair(halfDistance, fullDistance))
+            add(Pair(-halfDistance, fullDistance))
+        }
+        val secondPosArray = ArrayList<Pair<Float, Float>>().apply {
+            add(Pair(0.0f, 0.0f))
+            add(Pair(-fullDistance, 0.0f))
+            add(Pair(-halfDistance, -fullDistance))
+        }
+        val thirdPosArray = ArrayList<Pair<Float, Float>>().apply {
+            add(Pair(0.0f, 0.0f))
+            add(Pair(halfDistance, -fullDistance))
+            add(Pair(fullDistance, 0.0f))
+        }
 
         posArrayList.add(firstPosArray)
-
-        val secondPosArray = ArrayList<Pair<Float, Float>>()
-
-        secondPosArray.add(Pair(0.0f, 0.0f))
-        secondPosArray.add(Pair(-fullDistance, 0.0f))
-        secondPosArray.add(Pair(-halfDistance, -fullDistance))
-
         posArrayList.add(secondPosArray)
-
-        val thirdPosArray = ArrayList<Pair<Float, Float>>()
-
-        thirdPosArray.add(Pair(0.0f, 0.0f))
-        thirdPosArray.add(Pair(halfDistance, -fullDistance))
-        thirdPosArray.add(Pair(fullDistance, 0.0f))
-
         posArrayList.add(thirdPosArray)
     }
 
-    private fun startLoading() {
+
+    // Animation controls
+    override fun startAnimation() {
+        animationStopped = false
+
+        clearPreviousAnimations()
 
         val firstCircleAnim = getTranslateAnim(1)
         firstCircle.startAnimation(firstCircleAnim)
@@ -205,29 +235,32 @@ class AllianceLoader : LinearLayout, LoaderContract {
         val secondCircleAnim = getTranslateAnim(2)
         secondCircle.startAnimation(secondCircleAnim)
 
-        val thirdCircleAnim = getTranslateAnim(3)
-
-        thirdCircleAnim.setAnimationListener(object : Animation.AnimationListener {
-
-            override fun onAnimationEnd(p0: Animation?) {
+        val thirdCircleAnim = getTranslateAnim(3).apply {
+            onAnimationEnd {
                 step++
                 if (step > 2) {
                     step = 0
                 }
-                startLoading()
+                if (!animationStopped) startAnimation()
             }
-
-            override fun onAnimationRepeat(p0: Animation?) {
-            }
-
-            override fun onAnimationStart(p0: Animation?) {
-            }
-
-        })
+        }
 
         thirdCircle.startAnimation(thirdCircleAnim)
     }
 
+    override fun stopAnimation() {
+        animationStopped = true
+
+        clearPreviousAnimations()
+    }
+
+    override fun clearPreviousAnimations() {
+        firstCircle.clearAnimation()
+        secondCircle.clearAnimation()
+        thirdCircle.clearAnimation()
+    }
+
+    // Animations
     private fun getTranslateAnim(circleCount: Int): TranslateAnimation {
 
         var nextStep = step + 1
@@ -241,12 +274,32 @@ class AllianceLoader : LinearLayout, LoaderContract {
         val toXPos = posArrayList[circleCount - 1][nextStep].first
         val toYPos = posArrayList[circleCount - 1][nextStep].second
 
-        val transAnim = TranslateAnimation(fromXPos, toXPos, fromYPos, toYPos)
-        transAnim.duration = animDuration.toLong()
-        transAnim.fillAfter = true
-        transAnim.interpolator = AccelerateDecelerateInterpolator()
-        transAnim.repeatCount = 0
+        return TranslateAnimation(fromXPos, toXPos, fromYPos, toYPos).apply {
+            duration = animDuration.toLong()
+            fillAfter = true
+            interpolator = AccelerateDecelerateInterpolator()
+            repeatCount = 0
+        }
+    }
 
-        return transAnim
+    // Overrides
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+        if (calWidthHeight == 0) {
+            calWidthHeight = (2 * dotsRadius * distanceMultiplier) + strokeWidth
+        }
+        setMeasuredDimension(calWidthHeight, calWidthHeight)
+    }
+
+    override fun onVisibilityChanged(changedView: View, visibility: Int) {
+        super.onVisibilityChanged(changedView, visibility)
+        if (!toggleOnVisibilityChange) return
+
+        if (visibility != VISIBLE) {
+            stopAnimation()
+        } else {
+            startAnimation()
+        }
     }
 }
